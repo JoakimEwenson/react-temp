09a3149ab26bca635f326f6c68ac17fc49678eb9
Joakim Ewenson <joakim@ewenson.se>
2020-08-16 13:10:33 +0200

    More minor fixes...

--- src/Components/PageNav.js
+++ src/Components/PageNav.js
@@ -2,14 +2,13 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Navbar, Nav, NavLink } from "react-bootstrap";
 import { LinkContainer } from "react-router-bootstrap";
-import { ReactComponent as Logo } from "../sun-solid.svg";
 
 export default function PageNav() {
   return (
     <Navbar bg="dark" variant="dark" expand="lg" fluid="true" className="bg-dark">
       <Navbar.Brand>
         <Link to="" className="navbar-brand">
-        <i class="fas fa-sun mx-1"></i>
+        <i class="fas fa-sun mr-2"></i>
           <b>temperatur.nu</b>
         </Link>
       </Navbar.Brand>
@@ -17,16 +16,16 @@ export default function PageNav() {
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mr-auto" defaultActiveKey="/">
           <LinkContainer to="/favoriter">
-            <NavLink><i className="fas fa-star mx-1"></i>Favoriter</NavLink>
+            <NavLink><i className="fas fa-star mr-2"></i>Favoriter</NavLink>
           </LinkContainer>
           <LinkContainer to="/narliggande">
-            <NavLink><i class="fas fa-location-arrow mx-1"></i>Närliggande</NavLink>
+            <NavLink><i class="fas fa-location-arrow mr-2"></i>Närliggande</NavLink>
           </LinkContainer>
           <LinkContainer to="/platslista">
-            <NavLink><i class="fas fa-bars mx-1"></i>Alla mätpunkter</NavLink>
+            <NavLink><i class="fas fa-bars mr-2"></i>Alla mätpunkter</NavLink>
           </LinkContainer>
           <LinkContainer to="/om">
-            <NavLink><i class="fas fa-question-circle mx-1"></i>Om tjänsten</NavLink>
+            <NavLink><i class="fas fa-question-circle mr-2"></i>Om tjänsten</NavLink>
           </LinkContainer>
         </Nav>
       </Navbar.Collapse>
