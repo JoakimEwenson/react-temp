73db2f00a4d3496fb175aff93024d773480f606b
Joakim Ewenson <joakim@ewenson.se>
2020-08-16 15:02:08 +0200

    Minor fixes

--- README.md
+++ README.md
@@ -1,19 +1,22 @@
-# Temperatur.nu-webbapp skriven med React
+## Temperatur.nu-webbapp skriven med React
 _Av Joakim Ewenson, augusti 2020_
 
-## Beskrivning
-En simpel webbapp skriven i React med Bootstrap för att hämta temperaturer från nätverket av mätstationer på temperatur.nu.
+### Beskrivning
+En simpel webbapp skriven i React med Bootstrap som stilmallsramverk. Syftet är att hämta temperaturer från nätverket av mätstationer på temperatur.nu.
 
-## Funktioner
+### Funktioner
 * Lista över närliggande stationer
 * Lista över samtliga stationer, sökbar i realtid
 * Detaljerad vy över station och dess information
+* Favoritlista baserat på Javascripts LocalStorage
+* Möjlighet att välja en mätstation som hemskärm vid återbesök
 
-## Kommande
-* Fungerande favoriter baserat på Javascripts LocalStorage
+### Kommande
+* Möjlighet att sortera på högsta/lägsta temperatur
+* Möjlighet att söka närmsta station via positioneringstjänst
 * Karta på detaljvyn
 * Närliggande stationer på detaljvyn
 * Kartvy
 
-## Övrigt
+### Övrigt
 Information om temperatur.nu och dess API finns på adressen http://wiki.temperatur.nu/index.php/API
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -7,11 +7,16 @@ export default function About() {
     <Card className="my-3">
       <Card.Body>
         <h2>Temperatur.nu-webbapp skriven med React</h2>
-        <p className="text-muted"><em>Av <a href="https://www.ewenson.se">Joakim Ewenson</a>, augusti 2020</em></p>
+        <p className="text-muted">
+          <em>
+            Av <a href="https://www.ewenson.se">Joakim Ewenson</a>, augusti 2020
+          </em>
+        </p>
         <h4>Beskrivning</h4>
         <p>
-          En simpel webbapp skriven i React med Bootstrap för att hämta
-          temperaturer från nätverket av mätstationer på temperatur.nu.
+          En simpel webbapp skriven i React med Bootstrap som stilmallsramverk.
+          Syftet är att hämta temperaturer från nätverket av mätstationer på
+          <a href="https://temperatur.nu/">temperatur.nu</a>.
         </p>
 
         <h4>Funktioner</h4>
@@ -19,11 +24,14 @@ export default function About() {
           <li>Lista över närliggande stationer</li>
           <li>Lista över samtliga stationer, sökbar i realtid</li>
           <li>Detaljerad vy över station och dess information</li>
+          <li>Favoritlista baserat på Javascripts LocalStorage</li>
+          <li>Möjlighet att välja en mätstation som hemskärm vid återbesök</li>
         </ul>
 
         <h4>Kommande</h4>
         <ul>
-          <li>Fungerande favoriter baserat på Javascripts LocalStorage</li>
+          <li>Möjlighet att sortera på högsta/lägsta temperatur</li>
+          <li>Möjlighet att söka närmsta station via positioneringstjänst</li>
           <li>Karta på detaljvyn</li>
           <li>Närliggande stationer på detaljvyn</li>
           <li>Kartvy</li>
@@ -31,10 +39,18 @@ export default function About() {
 
         <h4>Övrigt</h4>
         <p>
-          Information om temperatur.nu och dess API finns på adressen{" "}
+          Information om temperatur.nu och dess API finns på adressen&nbsp;
           <a href="http://wiki.temperatur.nu/index.php/API">
             http://wiki.temperatur.nu/index.php/API
           </a>
+          .
+        </p>
+        <p>
+          Vill du följa projektet på Github så hittar du repot på&nbsp;
+          <a href="https://github.com/JoakimEwenson/react-temp/">
+            https://github.com/JoakimEwenson/react-temp/
+          </a>
+          .
         </p>
       </Card.Body>
     </Card>
