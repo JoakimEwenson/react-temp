4bc242a225bdfd8ae4189ca088a901302497e139
Joakim Ewenson <joakim@ewenson.se>
2020-08-17 12:21:38 +0200

    Minor UI fix

--- src/Components/PageFooter.js
+++ src/Components/PageFooter.js
@@ -2,17 +2,17 @@ import React from "react";
 
 export default function PageFooter() {
   return (
-    <div className="bg-dark mx-auto text-center fixed-bottom sticky-bottom mt-3 p-1">
+    <div className="bg-dark mx-auto text-center fixed-bottom sticky-bottom">
       <a href="https://github.com/JoakimEwenson/react-temp">
-        <i className="fab fa-github footerIcon px-2"></i>
+        <i className="fab fa-github footerIcon p-2"></i>
       </a>
       <a href="https://twitter.com/JoakimEwenson">
-        <i class="fab fa-twitter footerIcon px-2"></i>
+        <i class="fab fa-twitter footerIcon p-2"></i>
       </a>
       <a href="https://www.linkedin.com/in/joakim-ewenson-a1586377/">
-        <i class="fab fa-linkedin-in footerIcon px-2"></i>
+        <i class="fab fa-linkedin-in footerIcon p-2"></i>
       </a>
-      <a href="https://www.ewenson.se"><i class="fab fa-wordpress footerIcon px-2"></i></a>
+      <a href="https://www.ewenson.se"><i class="fab fa-wordpress footerIcon p-2"></i></a>
     </div>
   );
 }
