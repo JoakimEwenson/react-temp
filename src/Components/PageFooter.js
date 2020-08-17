ffb01743d650e373093fe4b9a8fa9edf724287d6
Joakim Ewenson <joakim@ewenson.se>
2020-08-17 12:24:45 +0200

    Another UI fix

--- src/Components/PageFooter.js
+++ src/Components/PageFooter.js
@@ -4,15 +4,15 @@ export default function PageFooter() {
   return (
     <div className="bg-dark mx-auto text-center fixed-bottom sticky-bottom">
       <a href="https://github.com/JoakimEwenson/react-temp">
-        <i className="fab fa-github footerIcon p-2"></i>
+        <i className="fab fa-github footerIcon m-2"></i>
       </a>
       <a href="https://twitter.com/JoakimEwenson">
-        <i class="fab fa-twitter footerIcon p-2"></i>
+        <i class="fab fa-twitter footerIcon m-2"></i>
       </a>
       <a href="https://www.linkedin.com/in/joakim-ewenson-a1586377/">
-        <i class="fab fa-linkedin-in footerIcon p-2"></i>
+        <i class="fab fa-linkedin-in footerIcon m-2"></i>
       </a>
-      <a href="https://www.ewenson.se"><i class="fab fa-wordpress footerIcon p-2"></i></a>
+      <a href="https://www.ewenson.se"><i class="fab fa-wordpress footerIcon m-2"></i></a>
     </div>
   );
 }
