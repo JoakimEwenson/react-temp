627268605023259662e1d7c019a51ea435a136bb
Joakim Ewenson <joakim@ewenson.se>
2022-01-30 13:17:00 +0100

    Minor width restriction

--- src/Components/PageNav.js
+++ src/Components/PageNav.js
@@ -8,7 +8,7 @@ export default function PageNav() {
     <nav className="">
       <div className="mx-auto px-2 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
-          <div className="flex-1 flex items-center justify-between sm:items-stretch sm:justify-between">
+          <div className="flex-1 flex items-center justify-between sm:items-stretch sm:justify-between md:max-w-6xl mx-auto">
             <div className="flex-shrink-0 flex items-center">
               <Link
                 to="/"
